b133d673a87fa2e3633f33efb9af79d4cc11b101
Shoptronic <reinayromero@gmail.com>
2026-01-07 23:11:38 +0100

    sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener('fetch', function(event) {
-  // Este archivo vacío es suficiente para engañar a Chrome
-  // y que nos permita instalar la web como App.
-  event.respondWith(fetch(event.request));
-});
